db79fe0452ecadbdc23f9fb0bd96f68fc1796775
SYNAPSE <jetalicr@gmail.com>
2026-01-08 19:05:40 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,10 @@
-const CACHE_NAME = "buny-puzzle-v1";
+const cacheName = 'wordzen-v1';
+const assets = ['./', './index.html', './manifest.json', './192.png', './512.png'];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html"
-      ]);
-    })
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
